ce4ec4cb992c4515eb495d65cb8bf8d8d0127546
Dolly Aswin Harahap <dolly.aswin@gmail.com>
2012-12-27 14:39:19 +0700

    Add Orchid module. We will work on this controller

--- config/application.config.php
+++ config/application.config.php
@@ -2,6 +2,7 @@
 return array(
     'modules' => array(
         'Application',
+        'Orchid'
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
---
+++ module/Orchid/config/module.config.php
@@ -0,0 +1,18 @@
+<?php
+namespace Orchid;
+
+return array(
+    'controllers' => array(
+        'invokables' => array(
+         ),
+    ),
+    'router' => array(
+        'routes' => array(
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'api' => __DIR__ . '/../view',
+        ),
+    ),
+);
