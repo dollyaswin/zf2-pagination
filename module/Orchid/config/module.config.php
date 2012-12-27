3f589ef4b34a547e420426fe6e9b393ff87f42e5
Dolly Aswin Harahap <dolly.aswin@gmail.com>
2012-12-27 15:21:16 +0700

    Set up route to find/locations

--- module/Orchid/config/module.config.php
+++ module/Orchid/config/module.config.php
@@ -4,10 +4,25 @@ namespace Orchid;
 return array(
     'controllers' => array(
         'invokables' => array(
+            'Orchid\Controller\Country'  => 'Orchid\Controller\CountryController',
          ),
     ),
     'router' => array(
         'routes' => array(
+            'locations' => array(
+    			'type' => 'Segment', 
+     			'options' => array(
+                    'route'    => '/find/locations[/:page]',
+                    'constraints' => array(
+                        'page'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Orchid\Controller\Country',
+                        'action' => 'find',
+                    	'page' => 1
+                    ),
+    			)
+            ),
         ),
     ),
     'view_manager' => array(
